3beda97aa5e165942a35e1fcde29b129b65064cc
Brandon Crooks <crookrb@hotmail.com>
2025-03-30 14:44:05 -0400

    add Open Graph meta tags to index.html and format vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,28 @@
+/** @format */
+
 import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "nonadsp",
-    project: "javascript-react"
-  }), sentryVitePlugin({
-    org: "nonadsp",
-    project: "javascript-react"
-  })],
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: "nonadsp",
+      project: "javascript-react",
+    }),
+    sentryVitePlugin({
+      org: "nonadsp",
+      project: "javascript-react",
+    }),
+  ],
 
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 
   build: {
-    sourcemap: true
-  }
+    sourcemap: true,
+  },
 });
